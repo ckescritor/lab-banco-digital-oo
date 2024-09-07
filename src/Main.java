aa8e7bc34c1e5a3cda3d0c0b4aa2dac9cc41b10e
ckescritor <66878103+ckescritor@users.noreply.github.com>
2024-09-07 19:16:11 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
-
 public class Main {
 
-	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+    public static void main(String[] args) {
+        Cliente venilton = new Cliente();
+        venilton.setNome("Venilton");
+        venilton.setTelefone("+5511970707070");
+        venilton.setEmail("70@gmail.com");
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
-	}
+        Conta cc = new ContaCorrente(venilton);
+        Conta poupanca = new ContaPoupanca(venilton);
 
+        // Operações
+        cc.depositar(100);
+        cc.transferir(50, poupanca);
+        poupanca.depositar(30);
+
+        // Relatórios
+        cc.imprimirExtratoDetalhado();
+        poupanca.imprimirExtratoDetalhado();
+
+        Banco banco = new Banco("Banco Exemplo");
+        banco.adicionarConta(cc);
+        banco.adicionarConta(poupanca);
+
+        banco.imprimirRelatorioContas();
+    }
 }
